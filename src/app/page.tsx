08f4358f3a6fddb488231aca2a99009dad509b4f
Fa-biano <fbiano.souza@gmail.com>
2025-06-14 18:21:46 -0300

    feat: add cache revalidate to getDailyGame method

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,15 @@ import { Container } from '@/components/container'
 import Link from 'next/link'
 import Image from 'next/image'
 import { IGameResponse } from '@/interfaces'
+import { BsArrowRightSquare } from 'react-icons/bs'
 
 const getDailyGame = async (): Promise<IGameResponse> => {
   try{
-    const res = await fetch(`${process.env.NEXT_API_URL}/next-api/?api=game_day`)
-    return res.json();
+    const response = await fetch(
+      `${process.env.NEXT_API_URL}/next-api/?api=game_day`,
+      { next: { revalidate: 320 } }
+    )
+    return response.json();
   }catch(err){
     throw new Error("Failed to fetch data")
   }
@@ -17,19 +21,27 @@ export default async function Home() {
   console.log('dailyGame', dailyGame)
 
   return (
-    <main className="flex">
+    <main className="w-full">
       <Container>
         <h1 className="text-center font-bold text-xl mt-8 mb-5">Separamos um jogo exclusivo pra você</h1>
         <Link href={`/game/${dailyGame.id}`}>
           <section className="w-full bg-black rounded-lg">
-            <Image
-              src={dailyGame.image_url}
-              alt={dailyGame.title}
-              priority={true}
-              quality={100}
-              width={100}
-              height={100}
-            />
+            <div className="w-full max-h-96 h-96  relative rounded-lg">
+              <div className='absolute z-20 bottom-0 p-3 flex justify-center items-center gap-2'>
+                <p className='font-bold text-xl text-white'>{dailyGame.title}</p>
+                <BsArrowRightSquare size={24} color='#fff' />
+              </div>
+
+              <Image
+                src={dailyGame.image_url}
+                alt={dailyGame.title}
+                priority={true}
+                quality={100}
+                fill={true}
+                className='max-h-96 object-cover rounded-lg opacity-50 hover:opacity-100 transition-all duration-300'
+                sizes='(max-width: 768px) 100vw, (max-width: 1200px) 44vw'
+              />
+            </div>
           </section>
         </Link>
       </Container>
