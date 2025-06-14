7db6b8ebfbe9f894a209fd6a00615f052453da43
Fa-biano <fbiano.souza@gmail.com>
2025-06-14 17:55:16 -0300

    feat: create IGameResponse interface

---
+++ src/interfaces/index.ts
@@ -0,0 +1,9 @@
+export interface IGameResponse {
+  id: number,
+  title: string,
+  description: string,
+  image_url: string,
+  platforms: string[],
+  categories: string[],
+  data_release: string,
+}
