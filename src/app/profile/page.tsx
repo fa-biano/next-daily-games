8912ad7d993780178b551fe36392450fe88f3933
Fa-biano <fbiano.souza@gmail.com>
2025-06-15 15:41:52 -0300

    feat: add metadata to profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -3,6 +3,12 @@ import Image from 'next/image'
 import userImg from '../../../public/user.png'
 import { FaShareAlt } from 'react-icons/fa'
 import FavoriteGameCard from '@/components/favoriteGameCard'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: "Meu perfil - Daly Games sua plataforma de jogos!",
+  description: "Perfil Sujeito Programador | Daly Games sua plataforma de jogos!"
+}
 
 export default function Profile() {
   return (
